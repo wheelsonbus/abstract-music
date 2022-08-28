ab87c8ba22b5d6e29ae513f07f9794761c886d47
Caius Paoli <caius.paoli@gmail.com>
2022-08-28 14:28:43 -0400

    Added cover to albums

--- src/Album.js
+++ src/Album.js
@@ -1,7 +1,8 @@
 export default class Album {
-    constructor(title, year, tracks) {
+    constructor(title, year, cover, tracks) {
         this.title = title;
         this.year = year;
+        this.cover = cover;
         this.tracks = tracks;
     }
 }
