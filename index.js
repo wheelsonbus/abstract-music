aa9804303365c068a30888e9f8c98a1760209ada
Caius Paoli <caius.paoli@gmail.com>
2022-08-29 13:21:33 -0400

    Spotify layer can now obtain an artist, their albums, and their tracks using their spotify id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Artist from "./src/Artist.js";
 import Album from "./src/Album.js";
 import Track from "./src/Track.js";
+import Spotify from "./src/Spotify.js";
 
-export { Artist, Album, Track };
+export { Artist, Album, Track, Spotify };
---
+++ src/Spotify.js
@@ -0,0 +1,95 @@
+import fetch from "node-fetch";
+
+import Album from "./Album.js";
+import Artist from "./Artist.js";
+import Track from "./Track.js";
+
+export default class Spotify {
+    token;
+
+    constructor(clientId, clientSecret) {
+        return (async () => {
+            const response = await fetch(
+                "https://accounts.spotify.com/api/token",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        Authorization:
+                            "Basic " + btoa(clientId + ":" + clientSecret),
+                    },
+                    body: "grant_type=client_credentials",
+                }
+            );
+
+            this.token = (await response.json()).access_token;
+            return this;
+        })();
+    }
+
+    async getArtist(id) {
+        const artist = new Artist("Coldplay", []);
+        let response;
+
+        response = await fetch("https://api.spotify.com/v1/artists/" + id, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + this.token,
+            },
+        });
+
+        const data = await response.json();
+
+        return new Artist(data.name, await this.getArtistAlbums(data.id));
+    }
+
+    async getArtistAlbums(id) {
+        const response = await fetch(
+            "https://api.spotify.com/v1/artists/" +
+                id +
+                "/albums?limit=50&include_groups=album",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + this.token,
+                },
+            }
+        );
+
+        const data = (await response.json()).items;
+        let albums = [];
+        for (const album of data) {
+            albums.push(
+                new Album(
+                    album.name,
+                    album.release_date,
+                    album.images[0].url,
+                    await this.getAlbumTracks(album.id)
+                )
+            );
+        }
+        return albums;
+    }
+
+    async getAlbumTracks(id) {
+        const response = await fetch(
+            "https://api.spotify.com/v1/albums/" + id + "/tracks?limit=50",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + this.token,
+                },
+            }
+        );
+
+        const data = (await response.json()).items;
+        let tracks = [];
+        data.forEach((track) => {
+            tracks.push(new Track(track.name));
+        });
+        return tracks;
+    }
+}
